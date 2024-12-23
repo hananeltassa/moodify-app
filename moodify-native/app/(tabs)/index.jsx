592e8722e8283264d85c5bac1174319f42cd225e
hananeltassa <hananeltassa15@gmail.com>
2024-12-23 16:08:53 +0200

    Updated Files structure

--- moodify-native/app/(tabs)/index.jsx
+++
@@ -1,9 +0,0 @@
-import { Text, View } from "react-native";
-
-export default function Home() {
-  return (
-    <View className="flex-1 justify-center items-center bg-blue-500">
-      <Text className="text-white text-lg font-avenir-demi">Hello, NativeWind!</Text>
-    </View>
-  );
-}
--- moodify-native/constants/images.js
+++
@@ -1,5 +0,0 @@
-import  logoWhite  from "../assets/images/logo-white.png";
-import  logoSmall  from "../assets/images/logo-small.png";
-import  background  from "../assets/images/bg.png";
-
-export default { logoWhite, logoSmall, background };
