24f3df1ac91f31caea26bedb62665dce5d90ee26
hananeltassa <hananeltassa15@gmail.com>
2024-12-24 20:20:36 +0200

    Added text-detection Screen

--- moodify-native/app/(mood-detection)/text-detection.jsx
+++ moodify-native/app/(mood-detection)/text-detection.jsx
@@ -1,12 +1,32 @@
-import { Text, View } from 'react-native'
-import React from 'react'
+import React, { useState } from "react";
+import { View, Text } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import FormField from "../../components/FormField";
+import CustomButton from "../../components/CustomButton";
 
 export default function TextDetection() {
+  const [text, setText] = useState("");
+
+  const handleSubmit = () => {
+    console.log("Submitted text:", text);
+  };
+
   return (
-    <View>
-      <Text>textDetection</Text>
+    <View className="justify-start mt-6 px-2">
+        <FormField
+          title="Share Your Mood"
+          value={text}
+          placeholder="Type how you feel..."
+          handleChangeText={setText}
+          inputSize={20}
+          multiline
+          numberOfLines={4}
+          style={{
+            height: 100,
+            textAlignVertical: "top",
+            padding: 10,
+          }}
+        />
     </View>
-  )
+  );
 }
-
-
