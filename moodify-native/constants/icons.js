89e90a0587950cfa4694cfa5796dc5148f47edcc
hananeltassa <hananeltassa15@gmail.com>
2024-12-24 18:56:57 +0200

    Updated Icons

--- moodify-native/constants/icons.js
+++ moodify-native/constants/icons.js
@@ -6,7 +6,7 @@ import spotify from "../assets/icons/spotify.png";
 import eyeHide from "../assets/icons/eye-hide.png";
 import eye from "../assets/icons/eye.png";
 import voice from "../assets/icons/voice.png";
-import pen from "../assets/icons/text.png";
+import pen from "../assets/icons/pen.png";
 import face from "../assets/icons/face.png";
 
 export default {  plus, profile, logout, google, spotify, eyeHide, eye, voice, pen, face };
