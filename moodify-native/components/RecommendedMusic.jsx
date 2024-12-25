d02ce763edf409aeb8258f19ae1de7b44f9e5db0
hananeltassa <hananeltassa15@gmail.com>
2024-12-25 23:20:00 +0200

    Added recomendation Music Component

---
+++ moodify-native/components/RecommendedMusic.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+import { View, Text, FlatList, Image } from "react-native";
+
+export default function RecommendedMusic({ data, title }) {
+  return (
+    <View className="p-4 bg-black">
+      {/* Title */}
+      {title && <Text className="text-white text-xl font-Avenir-Bold mb-2">{title}</Text>}
+
+      {/* Horizontal List */}
+      <FlatList
+        data={data}
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => (
+          <View className="w-45 border mr-4">
+            {/* Playlist Image */}
+            <Image source={item.image} className="w-40 h-40"/>
+            
+            {/* Playlist Title */}
+            <Text
+              className="text-white text-base font-Avenir-Bold mt-3"
+              numberOfLines={1}
+            >
+              {item.title}
+            </Text>
+
+            {/* Playlist Subtitle */}
+            <Text
+              className="text-gray-400 text-sm font-avenir-regular"
+              numberOfLines={1}
+            >
+              {item.subtitle}
+            </Text>
+          </View>
+        )}
+      />
+    </View>
+  );
+}
