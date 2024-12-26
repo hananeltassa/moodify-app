4f3de3ae27382654e685d23d6a0aea857960e32a
hananeltassa <hananeltassa15@gmail.com>
2024-12-27 00:57:54 +0200

    Updated Playlist

--- moodify-native/app/playlist/[playlist].jsx
+++ moodify-native/app/playlist/[playlist].jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { View, Image, Text, SafeAreaView, TouchableOpacity } from "react-native";
+import { SafeAreaProvider, useSafeAreaInsets } from "react-native-safe-area-context";
 import { useLocalSearchParams, useRouter } from "expo-router";
 import { Ionicons } from "@expo/vector-icons";
 import images from "@/constants/images";
@@ -9,31 +10,32 @@ export default function Playlist() {
   const { playlist, playlistName } = useLocalSearchParams();
   const router = useRouter();
   const [isLiked, setIsLiked] = useState(false);
+  const insets = useSafeAreaInsets();
 
   const toggleLike = () => {
     setIsLiked(!isLiked);
   };
 
   return (
-    <SafeAreaView className="flex-1 bg-black p-4">
-      {/* Back Button */}
-      <View className="flex-row items-center mb-6 relative ">
+    <SafeAreaProvider>
+      <SafeAreaView
+        style={{
+          flex: 1,
+          paddingTop: insets.top,
+          paddingBottom: insets.bottom,
+          paddingHorizontal: 16,
+          backgroundColor: "black",
+        }}
+      >
         {/* Back Button */}
-        <TouchableOpacity
-          onPress={() => router.back()}
-          className="absolute ml-4"
-        >
-          <Ionicons name="chevron-back" size={28} color="white" />
-        </TouchableOpacity>
-
-        {/* Title */}
-        <View className="flex-1 items-center">
-          <Text className="text-white text-xl font-bold">Playlist</Text>
+        <View style={{ flexDirection: "row", alignItems: "center", marginBottom: 20 }}>
+          <TouchableOpacity onPress={() => router.back()} style={{ marginRight: 16 }}>
+            <Ionicons name="chevron-back" size={28} color="white" />
+          </TouchableOpacity>
         </View>
-      </View>
 
-      {/* Playlist Image */}
-      <View className="items-center mb-6">
+        {/* Playlist Image */}
+        <View className="items-center mb-6">
         <Image
           source={images.playlist}
           className="w-96 h-96"
@@ -41,32 +43,34 @@ export default function Playlist() {
         />
       </View>
 
-      {/* Playlist Title and Like Icon */}
-      <View className="pl-5 mb-2">
-        <Text className="text-white text-2xl font-bold">{playlistName}</Text>
-        <TouchableOpacity onPress={toggleLike}>
-          <Ionicons
-            name={isLiked ? "heart" : "heart-outline"}
-            size={28}
-            color={isLiked ? "#FF6100" : "#FFF"}
-          />
-        </TouchableOpacity>
-      </View>
+        {/* Playlist Title and Like Icon */}
+        <View className="flex-row items-center justify-between p-4 mb-2">
+          <Text className="text-white text-2xl font-Avenir-Bold">{playlistName}</Text>
+          <TouchableOpacity onPress={toggleLike}>
+            <Ionicons
+              name={isLiked ? "heart" : "heart-outline"}
+              size={28}
+              color={isLiked ? "#FF6100" : "#FFF"}
+            />
+          </TouchableOpacity>
+        </View>
 
-      {/* Music Items */}
-      <Music
-        title="Belong Together"
-        subtitle="Mark Ambor"
-        image={images.playlist}
-        onPress={() =>
-          router.push({
-            pathname: "/music/[music]",
-            params: { music: "some-music", title: "Music Title" },
-          })
-          
-        }
-        onMorePress={() => console.log("More options pressed")}
-      />
-    </SafeAreaView>
+        <View className="flex-1  mb-2">
+          {/* Music Items */}
+          <Music
+            title="Belong Together"
+            subtitle="Mark Ambor"
+            image={images.playlist}
+            onPress={() =>
+              router.push({
+                pathname: "/music/[music]",
+                params: { music: "some-music", title: "Music Title" },
+              })
+            }
+            onMorePress={() => console.log("More options pressed")}
+          />
+        </View>
+      </SafeAreaView>
+    </SafeAreaProvider>
   );
 }
