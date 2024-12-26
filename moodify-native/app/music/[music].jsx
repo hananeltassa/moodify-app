6df42be5ae68f157a241a04c26d43dca95fb9c2a
hananeltassa <hananeltassa15@gmail.com>
2024-12-26 17:17:47 +0200

    Added Image Music Screen

--- moodify-native/app/music/[music].jsx
+++ moodify-native/app/music/[music].jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { View, Image, Text, SafeAreaView, TouchableOpacity } from "react-native";
 import { Ionicons } from "@expo/vector-icons";
+import Slider from "@react-native-community/slider";
 import { useRouter } from "expo-router";
 
 export default function SongPage() {
@@ -15,9 +16,8 @@ export default function SongPage() {
   return (
     <SafeAreaView className="flex-1 bg-black p-4">
       <View className="flex-row items-center justify-between mb-4">
-
         <TouchableOpacity onPress={() => router.back()}>
-          <Ionicons name="chevron-down" size={28} color="white" />
+          <Ionicons name="chevron-back" size={28} color="white" />
         </TouchableOpacity>
 
         <Text className="text-white text-lg font-bold">Playlist Name</Text>
@@ -27,7 +27,13 @@ export default function SongPage() {
         </TouchableOpacity>
       </View>
 
-      
+      <View className="items-center my-6">
+        <Image
+          source={{ uri: "https://via.placeholder.com/300" }}
+          className="w-96 h-96 "
+        />
+      </View>
+
     </SafeAreaView>
   );
 }
