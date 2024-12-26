ffc8a68789859a72ac548d3a2c9abdde483673b8
hananeltassa <hananeltassa15@gmail.com>
2024-12-26 22:42:56 +0200

    Added SafeAreaView Challenege

--- moodify-native/app/(tabs)/challenge.jsx
+++ moodify-native/app/(tabs)/challenge.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { View, Text, ScrollView } from "react-native";
+import { View, Text, ScrollView, SafeAreaView } from "react-native";
 import ChallengeCard from "../../components/ChallengeCard";
 
 const mockChallenges = [
@@ -26,31 +26,33 @@ export default function ChallengeScreen() {
   };
 
   return (
-    <ScrollView className="flex-1 bg-black px-4 py-6">
-      {/* Title */}
-      <Text className="text-white font-bold text-3xl mb-2">
-        Challenges <Text className="text-yellow-500">🏆</Text>
-      </Text>
-      <Text className="text-gray-300 text-base mb-6">
-        Ready to conquer today's challenges, Hanan?
-      </Text>
+    <SafeAreaView className=" h-full">
+      <ScrollView className="flex-1 bg-black px-4 py-6">
+        {/* Title */}
+        <Text className="text-white font-bold text-3xl mb-2">
+          Challenges <Text className="text-yellow-500">🏆</Text>
+        </Text>
+        <Text className="text-gray-300 text-base mb-6">
+          Ready to conquer today's challenges, Hanan?
+        </Text>
 
-      {/* Subtitle */}
-      <Text className="text-white font-semibold text-lg mb-4">
-        Today's AI Coach Challenges <Text className="text-yellow-500">🏆</Text>
-      </Text>
+        {/* Subtitle */}
+        <Text className="text-white font-semibold text-lg mb-4">
+          Today's AI Coach Challenges <Text className="text-yellow-500">🏆</Text>
+        </Text>
 
-      {/* Challenge Cards */}
-      {mockChallenges.map((challenge) => (
-        <ChallengeCard
-          key={challenge.id}
-          title={challenge.title}
-          description={challenge.description}
-          tags={challenge.tags}
-          image={challenge.image}
-          onAction={() => handleChallengeAction(challenge.id)}
-        />
-      ))}
-    </ScrollView>
+        {/* Challenge Cards */}
+        {mockChallenges.map((challenge) => (
+          <ChallengeCard
+            key={challenge.id}
+            title={challenge.title}
+            description={challenge.description}
+            tags={challenge.tags}
+            image={challenge.image}
+            onAction={() => handleChallengeAction(challenge.id)}
+          />
+        ))}
+      </ScrollView>
+    </SafeAreaView>
   );
 }
