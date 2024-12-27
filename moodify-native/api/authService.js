1d16dc4151eb1e0683895caafc7f6cd70bfc1bbd
hananeltassa <hananeltassa15@gmail.com>
2024-12-27 18:00:13 +0200

    Updated login auth service

--- moodify-native/api/authService.js
+++ moodify-native/api/authService.js
@@ -1,13 +1,19 @@
 import axios from "axios";
 
-const API_URL="http://localhost:8080/api/users";
+const API_URL="http://11.11.11.12:8080/api/users/";
 
 export const loginUser = async (email, password) =>{
     try{
-        const response = await axios.post(`${API_URL}/login`, { email, password });
-
+        console.log("Payload:", { email, password });
+        const response = await axios.post(`http://11.11.11.12:8080/api/users/login`, { email, password });
+        console.log("API Response:", response.data);
         return response.data;
     } catch (error){
+        // console.error("Error Details:", {
+        //     message: error.message,
+        //     response: error.response?.data,
+        //     status: error.response?.status,
+        //   });
         const errorMsg = error.response?.data?.error || "An unexpected error occurred.";
         throw new Error(errorMsg);
     }
