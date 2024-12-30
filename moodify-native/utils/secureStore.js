3fe322cfad13e13a40330bbed83355731f8dcea7
hananeltassa <hananeltassa15@gmail.com>
2024-12-30 02:14:41 +0200

    Verified token stored

--- moodify-native/utils/secureStore.js
+++ moodify-native/utils/secureStore.js
@@ -3,6 +3,7 @@ import * as SecureStore from 'expo-secure-store';
 export const saveToken = async (key, value) => {
   try {
     await SecureStore.setItemAsync(key, value);
+    console.log(`Token saved with key "${key}":`, value);
   } catch (error) {
     console.error('Error saving token:', error);
   }
