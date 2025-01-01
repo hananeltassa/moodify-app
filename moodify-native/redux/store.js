e27edd9e463978623e2c9895a199d9310d0c1a16
hananeltassa <hananeltassa15@gmail.com>
2025-01-02 00:03:47 +0200

    Added playlistSlice to store

--- moodify-native/redux/store.js
+++ moodify-native/redux/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import userReducer from './slices/userSlice';
+import playlistReducer from './slices/playlistSlice';
 
 const store = configureStore({
     reducer: {
       user: userReducer,
+      playlists: playlistReducer,
     },
 });
   
