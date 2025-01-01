a4c35463a8b3d77dace79877351b454a864d6493
hananeltassa <hananeltassa15@gmail.com>
2025-01-02 00:00:16 +0200

    Updated PlaylistSlice added reducers

--- moodify-native/redux/slices/playlistSlice.js
+++ moodify-native/redux/slices/playlistSlice.js
@@ -6,6 +6,18 @@ const playlistSlice = createSlice({
         items: [],
         isFetched: false,
     },
+    reducers:{
+        setPlaylists: (state, action) => {
+            state.items= action.payload;
+            state.isFetched= true;
+        },
+        clearPlaylists: (state) => {
+            state.items=[];
+            state.isFetched= false;
+        },
+    },
 });
+
+export const { setPlaylists, clearPlaylists } = playlistSlice.actions;
 
 export default playlistSlice.reducer;
