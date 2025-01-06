42d190bfa6939f6e4a775ee0d6330884a13cab82
hananeltassa <hananeltassa15@gmail.com>
2025-01-06 21:44:47 +0200

    Updated layout

--- moodify-native/app/_layout.jsx
+++ moodify-native/app/_layout.jsx
@@ -80,7 +80,7 @@ export default function RootLayout() {
               <Stack.Screen name="index" options={{ headerShown: false }} />
               <Stack.Screen name="onboarding" options={{ headerShown: false }} />
               <Stack.Screen name="(auth)" options={{ headerShown: false }} />
-              <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+              <Stack.Screen name="(tabs)" options={{ headerShown: false, gestureEnabled: false }} />
               <Stack.Screen
                 name="music/[music]"
                 options={{
