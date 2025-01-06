d205298651022361bb5840216a217a7cd2c1e0d9
hananeltassa <hananeltassa15@gmail.com>
2025-01-06 13:40:25 +0200

    Updated library Added a usePlaylists hook

--- moodify-native/app/(tabs)/library.jsx
+++ moodify-native/app/(tabs)/library.jsx
@@ -1,92 +1,19 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { FlatList, View, Text } from "react-native";
 import { SafeAreaProvider, SafeAreaView, useSafeAreaInsets } from "react-native-safe-area-context";
 import Playlist from "../../components/Playlist";
 import { useRouter } from "expo-router";
-import { useSelector, useDispatch } from "react-redux";
-import { getToken } from "../../utils/secureStore";
-import { fetchSpotifyPlaylists } from "../../api/spotifyAuth";
-import { setPlaylists } from "../../redux/slices/playlistSlice";
 import LoadingScreen from "../../components/LoadingScreen";
-import images from "@/constants/images"
-
-import { createPlaylist, getUserPlaylists } from "../../api/playlistService";
+import images from "@/constants/images";
+import { useSelector } from "react-redux";
+import { usePlaylists } from "../../hooks/usePlaylists";
 
 export default function Library() {
   const router = useRouter();
   const insets = useSafeAreaInsets();
-  const dispatch = useDispatch();
-
-  const playlists = useSelector((state) => state.playlists.items);
-  const isFetched = useSelector((state) => state.playlists.isFetched);
   const user = useSelector((state) => state.user.user);
 
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(null);
-
-  const ensureDefaultPlaylistExists = async (jwtToken) => {
-    try {
-      const { playlists: fetchedPlaylists } = await getUserPlaylists(jwtToken);
-  
-      const likedSongsPlaylist = fetchedPlaylists.find((playlist) => playlist.is_default);
-      if (!likedSongsPlaylist) {
-        console.log("Creating default 'Liked Songs' playlist...");
-        await createPlaylist(jwtToken, "My Favorite Songs", true);
-  
-        // Fetch playlists again after creation
-        const updatedPlaylists = await getUserPlaylists(jwtToken);
-        dispatch(setPlaylists(updatedPlaylists.playlists));
-      } else {
-        dispatch(setPlaylists(fetchedPlaylists));
-      }
-    } catch (error) {
-      console.error("Error ensuring default playlist exists:", error);
-      throw error;
-    }
-  };
-  
-  const fetchPlaylists = async () => {
-    try {
-      setLoading(true);
-
-      const jwtToken = await getToken("jwtToken");
-      if (!jwtToken) {
-        throw new Error("User is not logged in.");
-      }
-
-      await ensureDefaultPlaylistExists(jwtToken);
-
-      if (user?.spotifyId) {
-        console.log("Fetching Spotify playlists...");
-        const spotifyPlaylists = await fetchSpotifyPlaylists(jwtToken);
-
-        // Combine Spotify playlists with backend playlists (avoiding duplicates)
-        const backendPlaylists = await getUserPlaylists(jwtToken);
-        const combinedPlaylists = [
-          ...backendPlaylists.playlists,
-          ...spotifyPlaylists.filter(
-            (spotifyPlaylist) =>
-              !backendPlaylists.playlists.some(
-                (backendPlaylist) => backendPlaylist.id === spotifyPlaylist.id
-              )
-          ),
-        ];
-
-        dispatch(setPlaylists(combinedPlaylists));
-      }
-    } catch (err) {
-      console.error("Error fetching playlists:", err);
-      setError(err.message || "Failed to load playlists. Please try again later.");
-    } finally {
-      setLoading(false);
-    }
-  };
-
-  useEffect(() => {
-    if (!isFetched) {
-      fetchPlaylists();
-    }
-  }, [user]);
+  const { playlists, loading, error } = usePlaylists(user);
 
   if (loading) {
     return <LoadingScreen message="Loading your playlists..." />;
@@ -120,15 +47,12 @@ export default function Library() {
           {/* Playlist List */}
           <FlatList
             data={playlists}
-            className="pt-4"
             keyExtractor={(item) => item.id}
             renderItem={({ item }) => (
               <Playlist
                 title={item.name}
                 subtitle={
-                  item.name === "My Favorite Songs"
-                    ? null 
-                    : `${item.totalTracks || 0} tracks`
+                  item.name === "My Favorite Songs" ? null : `${item.totalTracks || 0} tracks`
                 }
                 image={
                   item.images?.length > 0
@@ -148,12 +72,9 @@ export default function Library() {
                 }
               />
             )}
-            contentContainerStyle={{
-              flexGrow: 1,
-            }}
+            contentContainerStyle={{ flexGrow: 1 }}
             showsVerticalScrollIndicator={false}
           />
-
         </View>
       </SafeAreaView>
     </SafeAreaProvider>
