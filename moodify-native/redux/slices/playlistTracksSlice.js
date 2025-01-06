b6a154f695f6c8d634a6a6e340cf4e52e36a3868
hananeltassa <hananeltassa15@gmail.com>
2025-01-06 16:17:11 +0200

    Updated playlistTracks slice

--- moodify-native/redux/slices/playlistTracksSlice.js
+++ moodify-native/redux/slices/playlistTracksSlice.js
@@ -1,24 +1,41 @@
 import { createSlice } from "@reduxjs/toolkit";
 
+const initialState = {
+  tracks: {},
+  isFetched: {},
+};
+
 const playlistTracksSlice = createSlice({
   name: "playlistTracks",
-  initialState: {
-    tracks: {},
-    isFetched: {},
-  },
+  initialState,
   reducers: {
     setPlaylistTracks: (state, action) => {
       const { playlistId, tracks } = action.payload;
       state.tracks[playlistId] = tracks;
       state.isFetched[playlistId] = true;
     },
-    clearPlaylistTracks: (state) => {
-      state.tracks = {};
-      state.isFetched = {};
+    clearPlaylistTracks: (state, action) => {
+      const playlistId = action.payload;
+      delete state.tracks[playlistId];
+      state.isFetched[playlistId] = false;
+    },
+    addTrackToPlaylist: (state, action) => {
+      const { playlistId, track } = action.payload;
+      if (!state.tracks[playlistId]) state.tracks[playlistId] = [];
+      state.tracks[playlistId].push(track);
+    },
+    removeTrackFromPlaylist: (state, action) => {
+      const { playlistId, trackId } = action.payload;
+      state.tracks[playlistId] = state.tracks[playlistId].filter((t) => t.id !== trackId);
     },
   },
 });
 
-export const { setPlaylistTracks, clearPlaylistTracks } = playlistTracksSlice.actions;
+export const {
+  setPlaylistTracks,
+  clearPlaylistTracks,
+  addTrackToPlaylist,
+  removeTrackFromPlaylist,
+} = playlistTracksSlice.actions;
 
 export default playlistTracksSlice.reducer;
