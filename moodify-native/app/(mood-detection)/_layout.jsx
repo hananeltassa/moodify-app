64a4b9f9a221b17b7a3f01fc01b0c65627406c67
hananeltassa <hananeltassa15@gmail.com>
2025-01-13 21:03:15 +0200

    Added recommendedPlaylist

--- moodify-native/app/(mood-detection)/_layout.jsx
+++ moodify-native/app/(mood-detection)/_layout.jsx
@@ -54,6 +54,13 @@ export default function MoodDetectionLayout() {
           ...commonScreenOptions,
         }}
       />
+      <Stack.Screen
+        name="recommendedPlaylist"
+        options={{
+          title: "Your Mood, Your Melody",
+          ...commonScreenOptions,
+        }}
+      />
     </Stack>
   );
 }
