e5bcac0e32d3491139c04fe217374d72216f91e2
hananeltassa <hananeltassa15@gmail.com>
2025-01-13 21:23:30 +0200

    Added mood redux

--- moodify-native/redux/store.js
+++ moodify-native/redux/store.js
@@ -3,6 +3,7 @@ import userReducer from './slices/userSlice';
 import playlistReducer from './slices/playlistSlice';
 import playlistTracksReducer from "./slices/playlistTracksSlice";
 import playbackReducer from "./slices/playbackSlice";
+import moodReducer from './slices/moodSlice';
 
 const store = configureStore({
     reducer: {
@@ -10,6 +11,7 @@ const store = configureStore({
       playlists: playlistReducer,
       playlistTracks: playlistTracksReducer,
       playback: playbackReducer,
+      mood: moodReducer,
     },
 });
   
